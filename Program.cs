bc5ed526ed75169ba8570ff1e1e0f2e92acf1ebb
Budoc53 GG <viot.g.53@orange.fr>
2021-01-31 14:24:10 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NameSpace
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            VariableSizeIntArray myArray = new VariableSizeIntArray();
+            for (int k = 0; k < 4; k++)
+            {
+                LineOfArray maligne = new LineOfArray();
+                for (int j = 0; j < 6; j++)
+                {
+                    maligne.AddAvalue(j + k * 10);
+                }
+                myArray.AddLine(maligne);
+            }
+
+            showArray(myArray);
+
+            static void showArray(VariableSizeIntArray intArray)
+            {
+                int a = intArray.NumberOfLines;
+                int b = intArray.ListeOfLines[0].IntList.Count;
+                Console.WriteLine("Nb Lignes = " + a);
+                Console.WriteLine("Nb colonnes = " + b);
+                for (int lines = 0; lines < intArray.NumberOfLines; lines++)
+                {
+                    for (int place = 0; place < b; place++)
+                    {
+                        Console.Write(intArray.ListeOfLines[lines].IntList[place].ToString() + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ VariableSizeArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameSpace
+{
+    class LineOfArray
+    {
+        // Représente une ligne de tableau composée d'entiers
+        public List<int> IntList = new List<int>();
+        public int NumberOfLineValues;
+
+
+        public void AddAvalue(int myIntValue)
+        {
+            IntList.Add(myIntValue);
+            NumberOfLineValues = IntList.Count;
+        }
+    }
+
+    class VariableSizeIntArray
+    {
+        // représente un tableau coposé de lignes d'entiers
+        public int NumberOfLines;
+        public List<LineOfArray> ListeOfLines = new List<LineOfArray>();
+
+        public  void AddLine(LineOfArray myNewLine)
+        {
+            ListeOfLines.Add(myNewLine);
+            NumberOfLines = ListeOfLines.Count;
+        }
+
+        
+
+    }
+}
