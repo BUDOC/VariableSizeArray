c213f4fcb9bace280d92b51b1075a4f9bc928d1d
Budoc53 GG <viot.g.53@orange.fr>
2021-01-31 20:15:55 +0100

    Show the result when somebody assign a wrong place.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace NameSpace
         private static void Main()
         {
             // construit un tableau d'entiers de 4 lignes par 3 colonnes
-           
+
 
             // Affiche les valeurs du tableau
             void showArray(VariableSizeIntArray intArray)
@@ -25,15 +25,17 @@ namespace NameSpace
                     Console.WriteLine();
                 }
 
-            Console.WriteLine("Code d'erreur = " + intArray.GetErrorCodeNumber());                             
+                Console.WriteLine("Code d'erreur = " + intArray.GetErrorCodeNumber());
             }
             Console.WriteLine("How much rows for your array?");
-            string nl =Console.ReadLine();
+            string nl = Console.ReadLine();
             Console.WriteLine("How much lines for your array?");
             string nr = Console.ReadLine();
             VariableSizeIntArray myArray = new VariableSizeIntArray(Convert.ToInt32(nl), Convert.ToInt32(nr));
-            // affecte la valeur 53 en (1,0)
+            // affecte la valeur 56 en (1,2)
             myArray.SetValue(1, 2, 56);
+            showArray(myArray);
+            myArray.SetValue(Convert.ToInt32(nl)+1, 4, 56);
             showArray(myArray);
         }
     }
